26943f12eb3378b9445f5a5c2d6a0f4d6c7e70d7
xGoldi24 <xkourai@gmail.com>
2025-04-02 20:58:26 +0200

    Create roll.js

---
+++ api/roll.js
@@ -0,0 +1,10 @@
+export default function handler(req, res) {
+  const user = req.query.user || "Someone";
+  const roll = Math.floor(Math.random() * 1000) + 1;
+
+  if (roll === 103) {
+    res.send(`${user} rolled a 103! 🎉 You win a skin!`);
+  } else {
+    res.send(`${user} rolled a ${roll}. Better luck next time!`);
+  }
+}
